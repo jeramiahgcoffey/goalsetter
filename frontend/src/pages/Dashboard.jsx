d6ab3cf0d42b679abe75a5aa48520c8b1ed233b1
Jeramiah Coffey <jerrygcoffey@gmail.com>
2022-04-09 14:38:58 -0500

    Add goal UI, goalSlice, goalService, GoalForm

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,27 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import GoalForm from '../components/GoalForm';
+
 function Dashboard() {
-    return <div>Dashboard</div>;
+    const navigate = useNavigate();
+    const { user } = useSelector((state) => state.auth);
+
+    useEffect(() => {
+        if (!user) {
+            navigate('/login');
+        }
+    }, [user, navigate]);
+
+    return (
+        <>
+            <section className='heading'>
+                <h1>Welcome {user && user.name}</h1>
+                <p>Goals Dashboard</p>
+            </section>
+            <GoalForm />
+        </>
+    );
 }
 
 export default Dashboard;
