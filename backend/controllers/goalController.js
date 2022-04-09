de1321f00ae9c8e7d4dd994fcd4ce06d47b608d3
Jeramiah Coffey <jerrygcoffey@gmail.com>
2022-04-09 13:49:42 -0500

    Backend logic fix, removed unneccesary call to the db to get the user.id

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -39,16 +39,14 @@ const updateGoal = asyncHandler(async (req, res) => {
         throw new Error('Goal not found');
     }
 
-    const user = await User.findById(req.user.id);
-
     // Check for user
-    if (!user) {
+    if (!req.user) {
         res.status(401);
         throw new Error('User not found');
     }
 
     // Match authorized user to goal's user
-    if (goal.user.toString() !== user.id) {
+    if (goal.user.toString() !== req.user.id) {
         res.status(401);
         throw new Error('User not authorized');
     }
@@ -71,16 +69,14 @@ const deleteGoal = asyncHandler(async (req, res) => {
         throw new Error('Goal not found');
     }
 
-    const user = await User.findById(req.user.id);
-
     // Check for user
-    if (!user) {
+    if (!req.user) {
         res.status(401);
         throw new Error('User not found');
     }
 
     // Match authorized user to goal's user
-    if (goal.user.toString() !== user.id) {
+    if (goal.user.toString() !== req.user.id) {
         res.status(401);
         throw new Error('User not authorized');
     }
