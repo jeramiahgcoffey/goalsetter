942d4ad7daa4d1fae51d4522541196b30fdd7efe
Jeramiah Coffey <jerrygcoffey@gmail.com>
2022-04-09 13:44:04 -0500

    Logout, Login, Register UI functionality

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const colors = require('colors');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 connectDB();
@@ -11,6 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.use('/api/goals', require('./routes/goalRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,10 @@
 import { useState, useEffect } from 'react';
 import { FaSignInAlt } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { login, reset } from '../features/auth/authSlice';
+import Spinner from '../components/Spinner';
 
 function Login() {
     const [formData, setFormData] = useState({
@@ -8,6 +13,25 @@ function Login() {
     });
 
     const { email, password } = formData;
+
+    const naviagte = useNavigate();
+    const dispatch = useDispatch();
+
+    const { user, isLoading, isError, isSuccess, message } = useSelector(
+        (state) => state.auth
+    );
+
+    useEffect(() => {
+        if (isError) {
+            toast.error(message);
+        }
+
+        if (isSuccess || user) {
+            naviagte('/');
+        }
+
+        dispatch(reset());
+    }, [user, isError, isSuccess, message, naviagte, dispatch]);
 
     const onChange = (e) => {
         setFormData((prevState) => ({
@@ -18,7 +42,18 @@ function Login() {
 
     const onSubmit = (e) => {
         e.preventDefault();
+
+        const userData = {
+            email,
+            password,
+        };
+
+        dispatch(login(userData));
     };
+
+    if (isLoading) {
+        return <Spinner />;
+    }
 
     return (
         <>
