3a7d19026e4cee64bc59b180c94d39ad8c5a3763
Jeramiah <jeramiahcoffey.dev@gmail.com>
2023-03-11 14:26:52 -0600

    prepare for render deployment

--- server.js
+++ server.js
@@ -20,19 +20,19 @@ app.use('/api/users', require('./routes/userRoutes'));
 
 // Serve frontend
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.use(express.static(path.join(__dirname, '/client/build')));
 
-    app.get('*', (req, res) =>
-        res.sendFile(
-            path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
-        )
-    );
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'client', 'build', 'index.html')
+    )
+  );
 } else {
-    app.get('/', (req, res) => res.send('Please set to production'));
+  app.get('/', (req, res) => res.send('Please set to production'));
 }
 
 app.use(errorHandler);
 
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+  console.log(`Server started on port ${port}`);
 });
